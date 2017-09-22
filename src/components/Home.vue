97cf4e48131c9d2d28534d4b74040e6704f7149c
Laurence <laurence.j.squires@gmail.com>
2017-09-22 14:57:15 +0100

    Added other home page options

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="hero is-primary" :class="$route.name === 'Home' ? 'is-large': 'is-small'">
+    <div class="hero is-primary is-bold" :class="$route.name === 'Home' ? 'is-medium': 'is-small'">
       <div class="hero-body sizeable">
         <div class="container has-text-centered">
           <h1 class="title is-2">Search</h1>
@@ -21,7 +21,7 @@
                   </b-input>
 
 
-                  <button class="button is-success" :disabled="iota === null" type="submit" :submit.prevent="searchNow" @click="searchNow">Search</button>
+                  <button class="button is-primary is-outlined is-inverted" :disabled="iota === null" type="submit" :submit.prevent="searchNow" @click="searchNow">Search</button>
                 </b-field>
               </b-field>
             </form>
@@ -29,6 +29,30 @@
         </div>
       </div>
     </div>
+    <transition name="fade">
+    <div class="hero is-success is-bold" v-if="$route.name === 'Home'">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-2">Import Wallet</h1>
+          <div class="container has-text-centered">
+            <button class="button is-success is-inverted is-large is-outlined" :disabled="iota === null">Import</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </transition>
+    <transition name="fade">
+      <div class="hero is-info is-bold" v-if="$route.name === 'Home'">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title is-2">Create Wallet</h1>
+            <div class="container has-text-centered">
+              <button class="button is-info is-inverted is-large is-outlined" :disabled="iota === null">Create</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
 
     <router-view :iota="iota"></router-view>
 
@@ -81,5 +105,12 @@ export default {
 <style scoped>
 .sizeable {
   transition: all ease .25s;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.25s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0
 }
 </style>
