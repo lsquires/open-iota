76ce8ef402dc4274bfe3b1789dffbc48c23c0d48
Laurence <laurence.j.squires@gmail.com>
2017-09-27 20:16:45 +0100

    Added file saving, loading and decryption of wallet

--- src/components/CreateWallet.vue
+++ src/components/CreateWallet.vue
@@ -2,7 +2,8 @@
   <div>
     <div class="container has-text-centered">
       <div class="content">
-      <h1 class="title is-2">Create New Wallet</h1>
+        <h1 class="title is-2">Create New Wallet</h1>
+        <h1 class="subtitle is-6">This will generate a random seed and encrypt it using your password into a wallet.</h1>
         <form class="has-text-centered" @submit.prevent="createWallet">
             <b-field position="is-centered" :type="isValidPassword || this.userEntry === '' ? '' : 'is-danger'"
                      :message="isValidPassword || this.userEntry === '' ? '' : 'Password must be longer than 6 chars'">
@@ -12,6 +13,7 @@
             </b-field>
             <b-field>
               <button class="button is-large is-info is-outlined is-inverted"
+                      :class="{'is-loading': isLoading}"
                       :disabled="iota === null || !isValidPassword" type="submit">
                 Generate
               </button>
@@ -22,7 +24,14 @@
     <div v-if="generated" class="container has-text-centered">
       <div class="content">
         <p>Done!</p>
+        <p>Your wallet has now been generated, please remember to back it up. </p>
+        <p>If you lose the wallet or your password then your seed will not be recoverable.</p>
 
+        <a :href="downloadBlob" :download="downloadFilename">
+          <button class="button is-large is-info is-outlined is-inverted">
+          Download
+          </button>
+        </a>
       </div>
     </div>
   </div>
@@ -36,6 +45,7 @@
     data () {
       return {
         generated: false,
+        isLoading: false,
         userEntry: '',
         wallet: {
           key : {},
@@ -49,11 +59,18 @@
     computed: {
       isValidPassword () {
         return this.userEntry.length > 6
+      },
+      downloadBlob () {
+        let blob = new Blob([JSON.stringify(this.wallet)], {type: "text/plain;charset=utf-8"})
+        return window.URL.createObjectURL(blob);
+      },
+      downloadFilename () {
+        return `IOTA-WALLET-${(new Date ()).toJSON()}.keystore`
       }
     },
     methods: {
       createWallet () {
-
+        this.isLoading = true
         var crypto = require('crypto')
 
         const seedBuffer = crypto.randomBytes(128);
@@ -61,47 +78,45 @@
         const keylength = 32
         const keySalt = crypto.randomBytes(128);
         const keyGenAlgo = 'sha512'
-        const keyIterations = 20000
-        const key = crypto.pbkdf2Sync(this.userEntry, keySalt, keyIterations, keylength, keyGenAlgo);
+        const keyIterations = 262144
+        crypto.pbkdf2(this.userEntry,
+          keySalt, keyIterations, keylength,
+          keyGenAlgo, (err, key) => {
 
-        const cipherAlgo = 'aes-256-ctr'
-        const cipherIV =  crypto.randomBytes(16);
-        const cipher = crypto.createCipheriv(cipherAlgo, key, cipherIV)
-        const cipherText = cipher.update(seedBuffer)
+          if(err) {
+            this.isLoading = false;
+            console.log(err)
+            return;
+          }
+          const cipherAlgo = 'aes-256-ctr'
+          const cipherIV =  crypto.randomBytes(16);
+          const cipher = crypto.createCipheriv(cipherAlgo, key, cipherIV)
+          const cipherText = cipher.update(seedBuffer)
 
-        this.wallet.key.algo = keyGenAlgo
-        this.wallet.key.iter = keyIterations
-        this.wallet.key.salt = keySalt.toString('hex')
-        this.wallet.key.length = keylength
+          this.wallet.key.algo = keyGenAlgo
+          this.wallet.key.iter = keyIterations
+          this.wallet.key.salt = keySalt.toString('hex')
+          this.wallet.key.length = keylength
 
-        this.wallet.cipher.text = cipherText.toString('hex')
-        this.wallet.cipher.algo = cipherAlgo
-        this.wallet.cipher.iv = cipherIV.toString('hex')
+          this.wallet.cipher.text = cipherText.toString('hex')
+          this.wallet.cipher.algo = cipherAlgo
+          this.wallet.cipher.iv = cipherIV.toString('hex')
 
-        this.wallet.type = 'OPEN IOTA'
-        this.wallet.version = 1
-        this.wallet.uuid = crypto.randomBytes(32).toString('hex')
+          this.wallet.type = 'OPEN IOTA'
+          this.wallet.version = 1
 
-        console.log(seedBuffer.toString('hex'))
-        console.log("Before: "+this.bufToSeed(seedBuffer))
-        console.log(this.wallet)
-        this.walletToSeed(this.wallet,this.userEntry)
-        this.generated = true;
-        this.userEntry = ''
-      },
-      walletToSeed(wallet, password) {
-        var crypto = require('crypto')
+          const hash = crypto.createHash('sha512')
+          hash.update(Buffer.concat([ key, cipherText ]))
+          this.wallet.uuid = hash.digest('hex')
 
-        const key = crypto.pbkdf2Sync(password,
-          Buffer.from(wallet.key.salt, 'hex'),  wallet.key.iter,
-          wallet.key.length,  wallet.key.algo);
+          console.log(seedBuffer.toString('hex'))
+          console.log("Before: "+this.bufToSeed(seedBuffer))
+          console.log(this.wallet)
 
-        const decipher = crypto.createDecipheriv(wallet.cipher.algo,
-          key, Buffer.from(wallet.cipher.iv, 'hex'))
-        const seedBuffer = decipher.update(Buffer.from(wallet.cipher.text, 'hex'))
-
-        console.log(seedBuffer.toString('hex'))
-        console.log("Deciphered: "+this.bufToSeed(seedBuffer))
+          this.generated = true;
+          this.userEntry = ''
+          this.isLoading = false
+          });
       },
       bufToSeed (buffer) {
         let alphabet  = '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
--- src/components/ImportWallet.vue
+++ src/components/ImportWallet.vue
@@ -1,23 +1,129 @@
 <template>
   <div>
-    import
+    <div class="container has-text-centered">
+      <div class="content">
+        <h1 class="title is-2">Import Wallet</h1>
+        <h1 class="subtitle is-6">Choose your wallet and unlock it to view your balance, make transactions...</h1>
+        <b-field>
+          <b-upload v-model="droppedFile" drag-drop type="is-white">
+            <section class="section">
+              <div class="content has-text-centered">
+                <p>
+                  <b-icon
+                    icon="file_upload"
+                    size="is-large">
+                  </b-icon>
+                </p>
+                <p>Drop your wallet here or click and find it</p>
+              </div>
+            </section>
+          </b-upload>
+        </b-field>
+      </div>
+    </div>
+    <div v-if="loaded" class="container has-text-centered">
+
+      <div class="content">
+        <h1 class="title is-2">Please enter your password</h1>
+        <form class="has-text-centered" @submit.prevent="unlockWallet" >
+          <b-field position="is-centered" :type="isValidPassword || this.userEntry === '' ? '' : 'is-danger'"
+                   :message="passwordMessage">
+            <b-input placeholder="Type your wallet password"
+                     type="password" v-model="userEntry " password-reveal>
+            </b-input>
+          </b-field>
+          <b-field :message="failMessage">
+            <button class="button is-large is-info is-outlined is-inverted"
+                    :class="{'is-loading': isLoading}"
+                    :disabled="iota === null || !isValidPassword" type="submit">
+              Unlock
+            </button>
+          </b-field>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+
   export default {
     props: ['iota'],
 
     data () {
       return {
-
+        isLoading: false,
+        userEntry: '',
+        droppedFile: [],
+        wallet: {},
+        failMessage: ''
+      }
+    },
+    watch: {
+      droppedFile: function (file) {
+        if (file && file.length === 1) {
+          let reader = new window.FileReader()
+          let self = this;
+          reader.onload = function (event) {
+            self.wallet = JSON.parse(event.target.result)
+          }
+          reader.readAsText(file[0])
+        }
       }
     },
     computed: {
-
+      loaded () {
+        return this.wallet && this.wallet.key && this.wallet.cipher || false
+      },
+      isValidPassword () {
+        return this.userEntry.length > 6
+      },
+      passwordMessage () {
+        return this.isValidPassword || this.userEntry === '' ? '' : 'Password must be longer than 6 chars'
+      }
     },
     methods: {
+      unlockWallet() {
+        this.isLoading = true
+        var crypto = require('crypto')
 
+        const key = crypto.pbkdf2Sync(this.userEntry,
+          Buffer.from(this.wallet.key.salt, 'hex'),  this.wallet.key.iter,
+          this.wallet.key.length,  this.wallet.key.algo);
+
+        const hash = crypto.createHash('sha512')
+        hash.update(Buffer.concat([key, Buffer.from(this.wallet.cipher.text, 'hex')]))
+        const correct = (this.wallet.uuid === hash.digest('hex'))
+        if(correct) {
+
+          const decipher = crypto.createDecipheriv(this.wallet.cipher.algo,
+            key, Buffer.from(this.wallet.cipher.iv, 'hex'))
+          const seedBuffer = decipher.update(Buffer.from(this.wallet.cipher.text, 'hex'))
+
+          console.log("Deciphered: " + this.bufToSeed(seedBuffer))
+          this.isLoading = false
+        } else {
+          this.failMessage = 'Incorrect password'
+          this.userEntry = ''
+          this.isLoading = false
+        }
+      },
+      bufToSeed (buffer) {
+        let alphabet  = '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+        let seed = ''
+        let index = 0;
+        let current = 0;
+        while(seed.length < 81) {
+          current =+ buffer.readUInt8(index)
+          while(current >= 27) {
+            seed += alphabet[current % 27]
+            current = Math.floor(current / 27)
+          }
+          index++;
+        }
+        return seed
+      }
     }
   }
 
